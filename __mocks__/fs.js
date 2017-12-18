e037b444cedc867f1d7bc096f6cae1f26d06bfd7
Ranjan Shrestha <ranjan@sharingapples.com>
2017-12-18 18:46:07 +0545

    Create a fs mock with readdirSync, existsSync and readFileSync overrides

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,10 +1,42 @@
-const fs = jest.genMockFromModule('fs');
+const fs = require.requireActual('fs');
 
-const orig = fs.readFileSync;
-const mocks = {};
+const origExists = fs.existsSync;
+const origRead = fs.readFileSync;
+const origDir = fs.readdirSync;
 
-fs.__setMockContent = (path, content) => {
-  mocks[path] = content;
+const mockedContent = {};
+const mockedDirectory = {};
+
+fs.__setMockFiles = (path, files) => {
+  mockedDirectory[path] = files;
 };
 
-fs.readFileSync = path => mocks[path] || orig(path);
+fs.__setMockContent = (path, content) => {
+  mockedContent[path] = content;
+};
+
+fs.readdirSync = (path, options) => {
+  if (mockedDirectory[path]) {
+    return mockedDirectory[path];
+  }
+
+  return origDir(path, options);
+};
+
+fs.readFileSync = (path, options) => {
+  if (mockedContent[path]) {
+    return mockedContent[path];
+  }
+
+  return origRead(path, options);
+};
+
+fs.existsSync = (path) => {
+  if (mockedContent[path]) {
+    return mockedContent[path];
+  }
+
+  return origExists(path);
+};
+
+module.exports = fs;
