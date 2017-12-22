5de15ac6704c631ce5c482a25034ce8c50ccd463
Ranjan Shrestha <ranjan@sharingapples.com>
2017-12-22 16:18:35 +0545

    Use sample project based test

--- __tests__/foundation.spec.js
+++ __tests__/foundation.spec.js
@@ -1,10 +1,33 @@
+const path = require('path');
+const foundation = require('../src/foundation');
 
 describe('react-native foundation command', () => {
-  it('validate getLibHook', () => {
-
+  let cwd = '';
+  beforeAll(() => {
+    // Store the current working directory, before anything starts
+    cwd = process.cwd();
   });
 
-  it('must call native hook', () => {
+  afterEach(() => {
+    // Change back the current working directly to normal
+    process.chdir(cwd);
+  });
 
+  it('validate with the mock project', () => {
+    process.chdir(path.resolve(__dirname, 'mockProject'));
+
+    foundation([], {
+      getProjectConfig: () => ({
+        android: {
+          settingsGradlePath: path.resolve('android/settings.gradle'),
+          buildGradlePath: path.resolve('android/app/build.gradle'),
+          mainFilePath: path.resolve('android/app/MainApplication.java'),
+        },
+        ios: {
+          sourceDir: path.resolve('ios'),
+          pbxprojPath: path.resolve('ios', 'project.pbxproj'),
+        },
+      }),
+    });
   });
 });
