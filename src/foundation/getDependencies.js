d6e135c3b9714fc6ba39c3c0c79b7e47eee70f8f
Ranjan Shrestha <ranjan@sharingapples.com>
2017-12-25 15:39:55 +0545

    Include plist file for each library

--- src/foundation/getDependencies.js
+++ src/foundation/getDependencies.js
@@ -102,6 +102,7 @@ module.exports = function getDependencies(pkg) {
       package: dPkg,
       podspecs,
       androidPackages: lib.androidPackages,
+      plist: lib.plist,
     });
   });
 
--- src/foundation/getLibInfo.js
+++ src/foundation/getLibInfo.js
@@ -40,22 +40,39 @@ module.exports = function getLibHook(pkg) {
   };
 
   // If there is a hook defined within the library itself use that
-  if (pkg.foundation && pkg.foundation.hook) {
-    const hookPath = path.resolve('.', 'node_modules', pkg.name, pkg.foundation.hook);
+  if (pkg.foundation) {
+    // check for plist
+    if (pkg.foundation.plist) {
+      const plistPath = path.resolve('.', 'node_modules', pkg.name, pkg.foundation.plist);
+      if (fs.exists(plistPath)) {
+        library.plist = plistPath;
+      }
+    }
 
-    if (fs.existsSync(hookPath)) {
-      try {
-        // eslint-disable-next-line global-require, import/no-dynamic-require
-        const hook = require(hookPath);
-        return attachHook(library, hook);
-      } catch (err) {
-        throw new Error(`Error while retrieving hook for dependency ${pkg.name} from ${pkg.foundation.hook}`);
+    // Check for hook
+    if (pkg.foundation.hook) {
+      const hookPath = path.resolve('.', 'node_modules', pkg.name, pkg.foundation.hook);
+
+      if (fs.existsSync(hookPath)) {
+        try {
+          // eslint-disable-next-line global-require, import/no-dynamic-require
+          const hook = require(hookPath);
+          return attachHook(library, hook);
+        } catch (err) {
+          throw new Error(`Error while retrieving hook for dependency ${pkg.name} from ${pkg.foundation.hook}`);
+        }
       }
     }
   }
 
   // If we have defined hooks for this particular library then use that
   if (!pkg.foundation) {
+    // Check if there is plist file in our lib section
+    const plistPath = path.resolve(__dirname, '..', 'libs', pkg.name, 'RNFoundation-Info.plist');
+    if (fs.existsSync(plistPath)) {
+      library.plist = plistPath;
+    }
+
     // See if we have defined a hook for this particular dependency
     if (preDefinedHooks[pkg.name]) {
       return attachHook(library, preDefinedHooks[pkg.name]);
