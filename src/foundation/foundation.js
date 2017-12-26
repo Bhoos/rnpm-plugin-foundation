b365f5605b98dcf64bc6cf02b659bdfb8a5d827f
Ranjan Shrestha <ranjan@sharingapples.com>
2017-12-26 17:50:30 +0545

    Do not append '.' for app configurations in constants

--- src/foundation/foundation.js
+++ src/foundation/foundation.js
@@ -29,10 +29,8 @@ module.exports = function foundation(args, config) {
   const constants = platFormProcessors.reduce((res, p) => {
     const r = flatten(p.getConstants(), '.');
     const pconfig = flatten(p.getConfig(), '.');
-    // Also add all the configurations as root level constants
-    Object.keys(pconfig).forEach((k) => {
-      r[`.${k}`] = pconfig[k];
-    });
+
+    Object.assign(r, pconfig);
 
     return Object.assign(res, {
       [p.getName()]: r,
@@ -48,37 +46,4 @@ module.exports = function foundation(args, config) {
       p[stage]();
     });
   });
-
-  // // Create ios specific structure
-  // const iosProject = xcode.project(project.ios.pbxprojPath);
-  // iosProject.parseSync();
-  // const appDelegateItem = iosProject.pbxItemByComment('AppDelegate.m', 'PBXFileReference');
-  // const ios = {
-  //   app: iosFilter(pkg.foundation, 'app'),
-  //   subModules: iosFilter(pkg.foundation, 'sub-modules'),
-  //   constants: flatten(iosFilter(pkg.foundation, 'constants'), '.'),
-  //   folder: project.ios.sourceDir,
-  //   pbxProject: iosProject,
-  //   appDelegatePath: path.resolve(project.ios.sourceDir, appDelegateItem.path),
-  // };
-
-  // ios.appDelegate = generateAppDelegate(fs.readFileSync(ios.appDelegatePath).toString('utf-8'));
-
-  // // Create android specific structure
-  // const android = {
-  //   app: androidFilter(pkg.foundation, 'app'),
-  //   subModules: androidFilter(pkg.foundation, 'sub-modules'),
-  //   constants: flatten(androidFilter(pkg.foundation, 'constants'), '.'),
-  //   folder: project.android.sourceDir,
-  //   manifestPath: project.android.manifestPath,
-  //   projectGradlePath: project.android.settingsGradlePath.replace('settings.gradle', 'build.gradle'),
-  //   appGradlePath: project.android.buildGradlePath,
-  //   settingsGradlePath: project.android.settingsGradlePath,
-  //   assetsPath: project.android.assetsPath,
-  //   mainFilePath: project.mainFilePath,
-  //   mainActivityPath: project.mainFilePath.replace('MainApplication.java', 'MainActivity.java'),
-  // };
-
-  // android.mainApplication = generateMainApplication(fs.readFileSync(android.mainFilePath).toString('utf-8'));
-  // android.mainActivity = generateMainActivity(fs.readFileSync(android.mainActivityPath).toString('utf-8'));
 };
