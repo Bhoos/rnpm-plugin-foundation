6b923197da689f3322733d9c59ad2e9f93faa783
Ranjan Shrestha <ranjan@sharingapples.com>
2017-12-28 13:40:56 +0545

    Add a method to add xml attribute

--- src/foundation/code/editor/generic.js
+++ src/foundation/code/editor/generic.js
@@ -1,10 +1,35 @@
 const fs = require('fs');
+const escapeRegex = require('../../util/escapeRegex');
 
 module.exports = function createGenericHandler(file) {
   let content = fs.readFileSync(file).toString('utf-8');
+  function replaceContent(pos, replaceLength, replacement) {
+    content = content.substring(0, pos) +
+                replacement +
+                content.substr(pos + replaceLength);
+  }
+
+  function insertContent(pos, part) {
+    replaceContent(pos, 0, part);
+  }
 
   const generator = {
-
+    addAttribute(tag, name, defaultValue) {
+      const r = `<${escapeRegex(tag)}\\s+[^>]*${escapeRegex(name)}="([^"]*)"[^>]*>`;
+      const regex = new RegExp(r, 'mg');
+      const match = regex.exec(content);
+      if (match !== null) {
+        const p = match[0].indexOf(match[1]);
+        replaceContent(match.index + p, match[1].length, defaultValue);
+      } else {
+        // the attribute is not there in the manifest
+        const r2 = new RegExp(`<${escapeRegex(tag)}\\s+[^>]*>`, 'mg');
+        const m = r2.exec(content);
+        if (m !== null) {
+          insertContent(m.index + (m[0].length - 1), ` ${name}="${defaultValue}"`);
+        }
+      }
+    },
   };
 
   return {
