ee95ba45d2c80914edddb585973f4f3244ad932f
Ranjan Shrestha <ranjan@sharingapples.com>
2017-12-28 22:03:04 +0545

    Allow providing scheme

--- src/foundation/code/AndroidManifest.xml.js
+++ src/foundation/code/AndroidManifest.xml.js
@@ -13,13 +13,13 @@ module.exports = function androidManifest(file) {
       });
     });
 
-    editor.addMethod('link', (root, host, pathPrefix) => {
+    editor.addMethod('link', (root, scheme, host, pathPrefix) => {
       const intentFilter = root.node('application').node('activity').create('intent-filter');
       intentFilter.create('action', { 'android:name': 'android.intent.action.VIEW' });
       intentFilter.create('category', { 'android:name': 'android.intent.category.DEFAULT' });
       intentFilter.create('category', { 'android:name': 'android.intent.category.BROWSABLE' });
       intentFilter.create('data', {
-        'android:scheme': 'https',
+        'android:scheme': scheme,
         'android:host': host,
         'android:pathPrefix': pathPrefix,
       });
