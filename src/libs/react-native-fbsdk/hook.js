c85afa996dae3e99ac34bca403dca4000d7b2ff2
Ranjan Shrestha <ranjan@sharingapples.com>
2017-12-28 18:49:30 +0545

    Update libraries to update plist and manifest via hook

--- src/libs/react-native-fbsdk/hook.js
+++ src/libs/react-native-fbsdk/hook.js
@@ -1,5 +1,22 @@
 module.exports = {
-  android: ({ code }) => {
+  android: ({ code, manifest }) => {
+    manifest.metaData('com.facebook.sdk.ApplicationId', 'fb{{fb.app.id}}');
+    const appNode = manifest.node('application');
+    appNode.create('activity', {
+      'android:name': 'com.facebook.FacebookActivity',
+      'android:configChanges': 'keyboard|keyboardHidden|screenLayout|screenSize|orientation',
+      'android:label': '{{fb.app.name}}',
+    });
+
+    const intentFilter = appNode.create('activity', {
+      'android:name': 'com.facebook.CustomTabActivity',
+      'android:exported': 'true',
+    }).create('intent-filter');
+    intentFilter.create('action', { 'android:name': 'android.intent.action.VIEW' });
+    intentFilter.create('category', { 'android:name': 'android.intent.category.DEFAULT' });
+    intentFilter.create('category', { 'android:name': 'android.intent.category.BROWSABLE' });
+    intentFilter.create('data', { 'android:scheme': 'fb{{fb.app.id}}' });
+
     code.mainApplication.import('com.facebook.CallbackManager');
     code.mainApplication.import('com.facebook.FacebookSdk');
     code.mainApplication.import('com.facebook.reactnative.androidsdk.FBSDKPackage');
@@ -18,10 +35,18 @@ module.exports = {
     //   'sdkInitialize(getApplicationContext())'
     // ));
 
+    code.mainActivity.import('android.content.Intent');
     code.mainActivity.onActivityResult('MainApplication.callbackManager');
   },
 
-  ios: ({ code }) => {
+  ios: ({ code, plist }) => {
+    plist.link('fb{{fb.app.id}}');
+    plist.queriesSchemes(['fbapi', 'fb-messenger-share-api', 'fbauth2', 'fbshareextension']);
+    plist.set({
+      FacebookAppID: '{{fb.app.id}}',
+      FacebookDisplayName: '{{fb.app.name}}',
+    });
+
     code.appDelegate.import('<FBSDKCoreKit/FBSDKCoreKit.h>');
     code.appDelegate.didFinishLaunchingWithOptions('[FBSDKApplicationDelegate sharedInstance');
     code.appDelegate.openURL('[FBSDKApplicationDelegate sharedInstance]', (app, url, options) => `
--- src/libs/react-native/hook.js
+++ src/libs/react-native/hook.js
@@ -1,11 +1,34 @@
 // Use the hook to add overrides for submodule specific code changes
 
 module.exports = {
-  android: (config) => {
+  android: ({ code, subModules, manifest }) => {
+    // if (subModules.React && subModules.React.RCTLinking) {
+    //   const scheme = 'https';
+    //   const { host, pathPrefix } = subModules.React.RCTLinking;
 
+    //   code.androidManifest
+    //     .intentFilter(`${host}${pathPrefix}`, (n) => {
+    //       n.add('action', { 'android:name': 'android.intent.action.VIEW' });
+    //       n.add('category', { 'android:name': 'android.intent.category.DEFAULT' });
+    //       n.add('category', { 'android:name': 'android.intent.category.BROWSABLE' });
+    //       n.add('data', {
+    //         'android:scheme': scheme,
+    //         'android:host': host,
+    //         'android:pathPrefix': pathPrefix,
+    //       });
+    //     });
+    // }
   },
 
-  ios: (config) => {
+  ios: ({ subModules }) => {
+    // console.log(subModules);
+    // // Fix code for Linking submodule
+    // if (subModules.React && subModules.React.RCTLinking) {
+    //   const scheme = 'https';
+    //   const { host, pathPrefix } = subModules.React.RCTLinking;
 
+
+
+    // }
   },
 };
